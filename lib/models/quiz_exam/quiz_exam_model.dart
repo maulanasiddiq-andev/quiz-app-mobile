bd317533478b27cf9e9ef553dde85f83f8257b36
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-28 19:37:13 +0700

    fix: rename file quiz_exam

--- lib/models/quiz_exam/quiz_exam_model.dart
+++ lib/models/quiz_exam/quiz_exam_model.dart
@@ -5,6 +5,7 @@ class QuizExamModel {
   final String quizId;
   final List<QuestionExamModel> questions;
   final int questionCount;
+  final int duration;
   int trueAnswers;
   int wrongAnswers;
   int score;
@@ -14,6 +15,7 @@ class QuizExamModel {
     required this.quizId,
     required this.questions,
     required this.questionCount,
+    required this.duration,
     required this.trueAnswers,
     required this.wrongAnswers,
     required this.score
