d0363c3a942e492d12a0c19a3b6a74695840a590
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-01 05:44:21 +0700

    fix: create button for redirecting to quiz create page

--- lib/pages/quiz/quiz_list_page.dart
+++ lib/pages/quiz/quiz_list_page.dart
@@ -5,6 +5,7 @@ import 'package:quiz_app/notifiers/auth_notifier.dart';
 import 'package:quiz_app/notifiers/quiz/quiz_list_notifier.dart';
 import 'package:quiz_app/pages/auth/login_page.dart';
 import 'package:quiz_app/pages/quiz/quiz_detail_page.dart';
+import 'package:quiz_app/pages/quiz_create/quiz_detail_create_page.dart';
 
 class QuizListPage extends ConsumerStatefulWidget {
   const QuizListPage({super.key});
@@ -46,33 +47,50 @@ class _QuizListPageState extends ConsumerState<QuizListPage> {
         onRefresh: () => ref.read(quizListProvider.notifier).refreshDatas(),
         child: state.isLoading
             ? Center(child: CircularProgressIndicator(color: Colors.blue))
-            : ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                itemCount: state.quizzes.length,
-                itemBuilder: (context, index) {
-                  final quiz = state.quizzes[index];
-
-                  return GestureDetector(
-                    onTap: () {
+            : Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        itemCount: state.quizzes.length,
+                        itemBuilder: (context, index) {
+                          final quiz = state.quizzes[index];
+                    
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => QuizDetailPage(quizId: quiz.quizId)),
+                              );
+                            },
+                            child: Container(
+                              margin: EdgeInsets.only(bottom: 10),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(quiz.title, style: TextStyle(fontSize: 18)),
+                            ),
+                          );
+                        },
+                      ),
+                  ),
+                  TextButton(
+                    onPressed: () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => QuizDetailPage(quizId: quiz.quizId)),
+                        context, 
+                        MaterialPageRoute(
+                          builder: (context) => QuizDetailCreatePage()
+                        )
                       );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 10),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(quiz.title, style: TextStyle(fontSize: 18)),
-                    ),
-                  );
-                },
+                    }, 
+                    child: Text("Buat Kuis")
+                  )
+                ],
               ),
       ),
     );
