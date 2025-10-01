9110e9e34d20c4f17d5d71db32ec5fad2904d8cb
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-02 06:06:17 +0700

    feature: category service

---
+++ lib/services/category_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:quiz_app/constants/environment_contant.dart';
+import 'package:http/http.dart' as http;
+import 'package:quiz_app/exceptions/api_exception.dart';
+import 'package:quiz_app/models/quiz/category_model.dart';
+import 'package:quiz_app/models/responses/base_response.dart';
+import 'package:quiz_app/models/responses/search_responses.dart';
+
+class CategoryService {
+  static const storage = FlutterSecureStorage();
+  static String url = "${EnvironmentConstant.url}category/";
+
+  static Future<BaseResponse<SearchResponse<CategoryModel>>> getQuizzes(
+    int page,
+    int pageSize,
+  ) async {
+    final baseUri = Uri.parse(url);
+    final uri = baseUri.replace(
+      queryParameters: {
+        'page': page.toString(),
+        'pageSize': pageSize.toString(),
+      },
+    );
+
+    var token = await storage.read(key: 'token');
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer $token', 'Accept': 'application/json'},
+    );
+
+    final dynamic responseJson = jsonDecode(response.body);
+    final BaseResponse<SearchResponse<CategoryModel>> result = BaseResponse.fromJson(
+      responseJson,
+      (data) => SearchResponse.fromJson(
+        data,
+        (item) => CategoryModel.fromJson(item),
+      ),
+    );
+
+    if (result.succeed == false) throw ApiException(result.messages[0]);
+
+    return result;
+  }
+}
