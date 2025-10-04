5650f4f671cef36cbdb94ab1d1489b013165aebc
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-04 22:11:36 +0700

    feature: chart

--- lib/pages/quiz_history/quiz_history_detail_page.dart
+++ lib/pages/quiz_history/quiz_history_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quiz_app/components/custom_appbar_component.dart';
@@ -34,20 +35,56 @@ class _QuizHistoryDetailPageState extends ConsumerState<QuizHistoryDetailPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 10,
-                children: [
-                  Text(
-                    widget.quizHistory.user.name,
-                    style: TextStyle(
-                      fontSize: 20
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 10,
+                  children: [
+                    Text(
+                      widget.quizHistory.user.name,
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
                     ),
-                  ),
-                  Text("Jawaban benar: ${widget.quizHistory.trueAnswers} dari ${widget.quizHistory.questionCount}"),
-                  Text("Durasi pengerjaan: ${formatTime(widget.quizHistory.duration)}"),
-                  Text("Nilai: ${widget.quizHistory.score}")
-                ],
+                    Text("Jawaban benar: ${widget.quizHistory.trueAnswers} dari ${widget.quizHistory.questionCount}"),
+                    Text("Durasi pengerjaan: ${formatTime(widget.quizHistory.duration)}"),
+                    Text("Nilai: ${widget.quizHistory.score}"),
+                    Center(
+                      child: SizedBox(
+                        width: 300,
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: PieChart(
+                            PieChartData(
+                              sections: [
+                                PieChartSectionData(
+                                  color: Colors.blue,
+                                  value: widget.quizHistory.trueAnswers / widget.quizHistory.questionCount * 100,
+                                  title: "Benar",
+                                  radius: 80,
+                                  titleStyle: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white
+                                  )
+                                ),
+                                PieChartSectionData(
+                                  color: Colors.red,
+                                  value: widget.quizHistory.wrongAnswers / widget.quizHistory.questionCount * 100,
+                                  title: "Salah",
+                                  radius: 80,
+                                  titleStyle: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white
+                                  )
+                                ),
+                              ]
+                            )
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Padding(
