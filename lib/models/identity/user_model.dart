a362d7124aa3abfe8f70fd53487db292e87217fd
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-13 20:54:18 +0700

    fix: accept properties from API in user model

--- lib/models/identity/user_model.dart
+++ lib/models/identity/user_model.dart
@@ -1,16 +1,25 @@
 class UserModel {
+  final String userId;
   final String email;
   final String username;
   final String name;
+  final String? profileImage;
+  final String? coverImage;
 
   UserModel({
     required this.email,
     required this.name,
     required this.username,
+    required this.coverImage,
+    required this.profileImage,
+    required this.userId
   });
 
   UserModel.fromJson(Map<String, dynamic> json) :
     email = json['email'],
     name = json['name'],
-    username = json['username'];
+    username = json['username'],
+    userId = json['userId'],
+    profileImage = json['profileImage'],
+    coverImage = json['coverImage'];
 }
