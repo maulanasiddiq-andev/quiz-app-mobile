8628bab5de4985c67bb15bb1baf79a1ed286f5f4
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-22 05:47:17 +0700

    feature: admin panel

--- lib/pages/admin/admin_page.dart
+++ lib/pages/admin/admin_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/components/custom_appbar_component.dart';
+import 'package:quiz_app/pages/admin/user/user_list_page.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({super.key});
@@ -8,8 +10,76 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
+  final List<AdminPanelMenu> menus = [
+    AdminPanelMenu(
+      page: UserListPage(), 
+      icon: Icons.person, 
+      title: "User"
+    ),
+    AdminPanelMenu(
+      page: UserListPage(), 
+      icon: Icons.badge, 
+      title: "Role"
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final colors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: customAppbarComponent(
+        "Panel Admin",
+        backgroundColor: colors.primary,
+        foregroundColor: colors.onPrimary
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...menus.map((menu) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => menu.page)
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: colors.onSurface)
+                    )
+                  ),
+                  child: Row(
+                    spacing: 20,
+                    children: [
+                      Icon(menu.icon, size: 30),
+                      Text(
+                        menu.title,
+                        style: TextStyle(
+                          fontSize: 20
+                        ),  
+                      )
+                    ],
+                  ),
+                ),
+              );
+            })
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class AdminPanelMenu {
+  final IconData icon;
+  final String title;
+  final Widget page;
+
+  AdminPanelMenu({
+    required this.page,
+    required this.icon,
+    required this.title
+  });
 }
---
+++ lib/pages/admin/role/role_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoleListPage extends StatefulWidget {
+  const RoleListPage({super.key});
+
+  @override
+  State<RoleListPage> createState() => _RoleListPageState();
+}
+
+class _RoleListPageState extends State<RoleListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
