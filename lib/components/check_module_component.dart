ac24eb0b78b63405851dff3907440e37bd26d8b2
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-26 01:16:37 +0700

    feature: module constant and component for checking module permission

---
+++ lib/components/check_module_component.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/notifiers/auth_notifier.dart';
+
+class CheckModuleComponent extends ConsumerStatefulWidget {
+  final Widget child;
+  final String moduleName;
+  const CheckModuleComponent({super.key, required this.child, required this.moduleName});
+
+  @override
+  ConsumerState<CheckModuleComponent> createState() => _CheckModuleComponentState();
+}
+
+class _CheckModuleComponentState extends ConsumerState<CheckModuleComponent> {
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(authProvider);
+    final modules = state.token?.user?.role?.roleModules;
+
+    return modules != null && modules.any((module) => module.roleModuleName == widget.moduleName)
+      ? widget.child
+      : SizedBox.shrink();
+  }
+}
---
+++ lib/constants/module_constant.dart
@@ -0,0 +1,22 @@
+class ModuleConstant {
+  static String searchQuiz = "SearchQuiz";
+  static String detailQuiz = "DetailQuiz";
+  static String createQuiz = "CreateQuiz";
+  static String editQuiz = "EditQuiz";
+  static String deleteQuiz = "DeleteQuiz";
+  static String searchCategory = "SearchCategory";
+  static String detailCategory = "DetailCategory";
+  static String createCategory = "CreateCategory";
+  static String editCategory = "EditCategory";
+  static String deleteCategory = "DeleteCategory";
+  static String searchUser = "SearchUser";
+  static String detailUser = "DetailUser";
+  static String createUser = "CreateUser";
+  static String editUser = "EditUser";
+  static String deleteUser = "DeleteUser";
+  static String searchRole = "SearchRole";
+  static String detailRole = "DetailRole";
+  static String createRole = "CreateRole";
+  static String editRole = "EditRole";
+  static String deleteRole = "DeleteRole";
+}
