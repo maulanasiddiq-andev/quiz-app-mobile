84b5a4b6c4b150af623bd38c46cac2b0f1cae99d
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-26 07:28:27 +0700

    fix: implement module check component in user detail and role detail page

--- lib/pages/admin/role/role_detail_page.dart
+++ lib/pages/admin/role/role_detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/components/check_module_component.dart';
 import 'package:quiz_app/components/custom_appbar_component.dart';
 import 'package:quiz_app/components/custom_button_component.dart';
 import 'package:quiz_app/components/detail_field_component.dart';
+import 'package:quiz_app/constants/module_constant.dart';
 import 'package:quiz_app/notifiers/admin/role/role_detail_notifier.dart';
 import 'package:quiz_app/pages/admin/role/role_edit_page.dart';
 
@@ -30,94 +32,104 @@ class _RoleDetailPageState extends ConsumerState<RoleDetailPage> {
       appBar: customAppbarComponent(
         "Detail Role",
         backgroundColor: colors.primary,
-        foregroundColor: colors.onPrimary
+        foregroundColor: colors.onPrimary,
       ),
       body: state.isLoading || state.role == null
-      ? Center(
-          child: CircularProgressIndicator(color: colors.primary),
-        )
-      : Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 15,
-                      children: [
-                        DetailFieldComponent(fieldName: "Nama", content: state.role!.name),
-                        DetailFieldComponent(
-                          fieldName: "Deskripsi", 
-                          content: state.role!.description.isNotEmpty ? state.role!.description : "-"
-                        ),
-                        Column(
+          ? Center(child: CircularProgressIndicator(color: colors.primary))
+          : Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
+                          spacing: 15,
                           children: [
-                            Text(
-                              "Role Module",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold
-                              ),
+                            DetailFieldComponent(
+                              fieldName: "Nama",
+                              content: state.role!.name,
                             ),
-                            GridView.count(
-                              shrinkWrap: true,
-                              physics: NeverScrollableScrollPhysics(),
-                              padding: EdgeInsets.zero,
-                              crossAxisCount: 2,
-                              childAspectRatio: 5,
+                            DetailFieldComponent(
+                              fieldName: "Deskripsi",
+                              content: state.role!.description.isNotEmpty
+                                  ? state.role!.description
+                                  : "-",
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                ...state.role!.roleModules.map((roleModule) {
-                                  return Text(
-                                    roleModule.roleModuleName,
-                                    style: TextStyle(
-                                      fontSize: 20
-                                    ),
-                                  );
-                                })
+                                Text(
+                                  "Role Module",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                GridView.count(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  padding: EdgeInsets.zero,
+                                  crossAxisCount: 2,
+                                  childAspectRatio: 5,
+                                  children: [
+                                    ...state.role!.roleModules.map((
+                                      roleModule,
+                                    ) {
+                                      return Text(
+                                        roleModule.roleModuleName,
+                                        style: TextStyle(fontSize: 20),
+                                      );
+                                    }),
+                                  ],
+                                ),
                               ],
                             ),
                           ],
-                        )
-                      ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                spacing: 10,
-                children: [
-                  Expanded(
-                    child: CustomButtonComponent(
-                      onTap: () async {
-                        final result = await Navigator.of(context).push(
-                          MaterialPageRoute(builder: (_) => RoleEditPage(roleId: state.role!.roleId))
-                        );
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    spacing: 10,
+                    children: [
+                      CheckModuleComponent(
+                        moduleNames: [ModuleConstant.editRole],
+                        child: Expanded(
+                          child: CustomButtonComponent(
+                            onTap: () async {
+                              final result = await Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) =>
+                                      RoleEditPage(roleId: state.role!.roleId),
+                                ),
+                              );
 
-                        if (result != null && result == true) {
-                          notifier.getRoleById(widget.roleId);
-                        }
-                      }, 
-                      text: "Edit",
-                    ),
+                              if (result != null && result == true) {
+                                notifier.getRoleById(widget.roleId);
+                              }
+                            },
+                            text: "Edit",
+                          ),
+                        ),
+                      ),
+                      CheckModuleComponent(
+                        moduleNames: [ModuleConstant.deleteRole],
+                        child: Expanded(
+                          child: CustomButtonComponent(
+                            onTap: () {},
+                            text: "Hapus",
+                            isError: true,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: CustomButtonComponent(
-                      onTap: () {}, 
-                      text: "Hapus",
-                      isError: true,
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+              ],
+            ),
     );
   }
 }
--- lib/pages/admin/user/user_detail_page.dart
+++ lib/pages/admin/user/user_detail_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/components/check_module_component.dart';
 import 'package:quiz_app/components/custom_appbar_component.dart';
 import 'package:quiz_app/components/custom_button_component.dart';
 import 'package:quiz_app/components/detail_field_component.dart';
 import 'package:quiz_app/components/profile_image_component.dart';
+import 'package:quiz_app/constants/module_constant.dart';
 import 'package:quiz_app/notifiers/admin/user/user_detail_notifier.dart';
 import 'package:quiz_app/pages/admin/user/user_edit_page.dart';
 import 'package:quiz_app/utils/format_date.dart';
@@ -32,87 +34,114 @@ class _UserDetailPageState extends ConsumerState<UserDetailPage> {
       appBar: customAppbarComponent(
         "Detail User",
         backgroundColor: colors.primary,
-        foregroundColor: colors.onPrimary
+        foregroundColor: colors.onPrimary,
       ),
       body: state.isLoading || state.user == null
-      ? Center(
-          child: CircularProgressIndicator(color: colors.primary),
-        )
-      : Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Column(
-                      spacing: 15,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: ProfileImageComponent(
-                            profileImage: state.user?.profileImage,
-                            radius: 60,
-                          ),
+          ? Center(child: CircularProgressIndicator(color: colors.primary))
+          : Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          spacing: 15,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: ProfileImageComponent(
+                                profileImage: state.user?.profileImage,
+                                radius: 60,
+                              ),
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Nama",
+                              content: state.user!.name,
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Email",
+                              content: state.user!.email,
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Username",
+                              content: state.user!.username,
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Email Diverifikasi pada",
+                              content: state.user!.emailVerifiedTime != null
+                                  ? formatDate(state.user!.emailVerifiedTime!)
+                                  : "-",
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Terakhir Login",
+                              content: state.user!.lastLoginTime != null
+                                  ? formatDate(state.user!.lastLoginTime!)
+                                  : "-",
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Role",
+                              content: state.user!.role != null
+                                  ? state.user!.role!.name
+                                  : "-",
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Deskripsi",
+                              content: state.user!.description.isEmpty
+                                  ? "-"
+                                  : state.user!.description,
+                            ),
+                            DetailFieldComponent(
+                              fieldName: "Status",
+                              content: state.user!.recordStatus,
+                            ),
+                          ],
                         ),
-                        DetailFieldComponent(fieldName: "Nama", content: state.user!.name),
-                        DetailFieldComponent(fieldName: "Email", content: state.user!.email),
-                        DetailFieldComponent(fieldName: "Username", content: state.user!.username),
-                        DetailFieldComponent(
-                          fieldName: "Email Diverifikasi pada", 
-                          content: state.user!.emailVerifiedTime != null ? formatDate(state.user!.emailVerifiedTime!) : "-"
-                        ),
-                        DetailFieldComponent(
-                          fieldName: "Terakhir Login", 
-                          content: state.user!.lastLoginTime != null ? formatDate(state.user!.lastLoginTime!) : "-"
-                        ),
-                        DetailFieldComponent(
-                          fieldName: "Role", 
-                          content: state.user!.role != null ? state.user!.role!.name : "-"
-                        ),
-                        DetailFieldComponent(
-                          fieldName: "Deskripsi", 
-                          content: state.user!.description.isEmpty ? "-" : state.user!.description
-                        ),
-                        DetailFieldComponent(fieldName: "Status", content: state.user!.recordStatus),
-                      ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                spacing: 10,
-                children: [
-                  Expanded(
-                    child: CustomButtonComponent(
-                      onTap: () async {
-                        final result = await Navigator.of(context).push(
-                          MaterialPageRoute(builder: (_) => UserEditPage(userId: state.user!.userId))
-                        );
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    spacing: 10,
+                    children: [
+                      CheckModuleComponent(
+                        moduleNames: [ModuleConstant.editUser],
+                        child: Expanded(
+                          child: CustomButtonComponent(
+                            onTap: () async {
+                              final result = await Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) =>
+                                      UserEditPage(userId: state.user!.userId),
+                                ),
+                              );
 
-                        if (result != null && result == true) {
-                          notifier.getUserById(widget.userId);
-                        }
-                      }, 
-                      text: "Edit",
-                    ),
+                              if (result != null && result == true) {
+                                notifier.getUserById(widget.userId);
+                              }
+                            },
+                            text: "Edit",
+                          ),
+                        ),
+                      ),
+                      CheckModuleComponent(
+                        moduleNames: [ModuleConstant.deleteUser],
+                        child: Expanded(
+                          child: CustomButtonComponent(
+                            onTap: () {},
+                            text: "Hapus",
+                            isError: true,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: CustomButtonComponent(
-                      onTap: () {}, 
-                      text: "Hapus",
-                      isError: true,
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+              ],
+            ),
     );
   }
 }
