55c689545406830860126207d4bc1aab4640612e
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-11-01 13:23:40 +0700

    feature: profile page

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/components/custom_appbar_component.dart';
+import 'package:quiz_app/components/detail_field_component.dart';
+import 'package:quiz_app/components/profile_image_component.dart';
+import 'package:quiz_app/notifiers/auth_notifier.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends ConsumerStatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  ConsumerState<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends ConsumerState<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final state = ref.watch(authProvider);
+
+    return Scaffold(
+      appBar: CustomAppbarComponent(title: "Profile"),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    spacing: 15,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: ProfileImageComponent(
+                          profileImage: state.token?.user?.profileImage,
+                          radius: 60,
+                        ),
+                      ),
+                      DetailFieldComponent(
+                        fieldName: "Nama",
+                        content: state.token?.user?.name,
+                      ),
+                      DetailFieldComponent(
+                        fieldName: "Email",
+                        content: state.token?.user?.email,
+                      ),
+                      DetailFieldComponent(
+                        fieldName: "Username",
+                        content: state.token?.user?.username,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
