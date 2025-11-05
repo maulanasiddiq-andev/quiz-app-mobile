15f544b5e53e43db778cf1f5520db2a3aa5e2673
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-11-06 05:28:50 +0700

    fix: load more, change sort dir, and search for select data dialog

--- lib/components/select_data_modal.dart
+++ lib/components/select_data_modal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/components/search_sort_component.dart';
 import 'package:quiz_app/notifiers/select_data_notifier.dart';
 
 class SelectDataModal extends ConsumerStatefulWidget {
@@ -11,21 +12,67 @@ class SelectDataModal extends ConsumerStatefulWidget {
 }
 
 class _SelectDataModalState extends ConsumerState<SelectDataModal> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 50) {
+        final notifier = ref.read(selectDataProvider(widget.data).notifier);
+        notifier.loadMoreDatas();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(selectDataProvider(widget.data));
+    final colors = Theme.of(context).colorScheme;
+    final notifier = ref.read(selectDataProvider(widget.data).notifier);
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      child: SizedBox(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (state.isLoading) 
-              Center(child: CircularProgressIndicator())
-            else
-              ListView(
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.6
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: colors.onSurface)
+              )
+            ),
+            child: Text(
+              "Pilih ${widget.data}",
+              style: TextStyle(
+                fontSize: 16
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          SearchSortComponent(
+            feature: widget.data, 
+            search: state.search, 
+            sortDir: state.sortDir, 
+            onSearchChanged: (value) {
+              notifier.searchDatas(value);
+            }, 
+            onSortDirChanged: (value) {
+              notifier.changeSortDir(value);
+            }
+          ),
+          if (state.isLoading) 
+            Center(child: CircularProgressIndicator())
+          else
+            Expanded(
+              child: ListView(
                 shrinkWrap: true,
+                controller: scrollController,
                 children: [
                   ...state.datas.map((data) {
                     return GestureDetector(
@@ -33,8 +80,13 @@ class _SelectDataModalState extends ConsumerState<SelectDataModal> {
                       onTap: () {
                         Navigator.of(context).pop(data);
                       },
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                        // decoration: BoxDecoration(
+                        //   border: Border(
+                        //     bottom: BorderSide(color: colors.onSurface)
+                        //   )
+                        // ),
                         child: Text(
                           data.name,
                           style: TextStyle(
@@ -43,11 +95,17 @@ class _SelectDataModalState extends ConsumerState<SelectDataModal> {
                         ),
                       ),
                     );
-                  })
+                  }),
+                  if (state.isLoadingMore)
+                    Center(
+                      child: CircularProgressIndicator(
+                        color: colors.primary,
+                      ),
+                    )
                 ],
-              )
-          ],
-        ),
+              ),
+            )
+        ],
       ),
     );
   }
