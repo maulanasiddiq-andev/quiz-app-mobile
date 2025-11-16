2e88d488ccf57b1ea016372fa2a55bd0dec5d2a7
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-11-17 06:07:13 +0700

    fix: create a single dio instance

--- lib/interceptor/client_settings.dart
+++ lib/interceptor/client_settings.dart
@@ -3,17 +3,23 @@ import 'package:quiz_app/constants/environment_contant.dart';
 import 'package:quiz_app/interceptor/jwt_interceptor.dart';
 
 class ClientSettings {
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: EnvironmentConstant.url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-    )
-  );
+  static final ClientSettings _instance = ClientSettings._internal();
 
-  ClientSettings() {
+  factory ClientSettings() => _instance;
+
+  late final Dio _dio;
+
+  ClientSettings._internal() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: EnvironmentConstant.url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+
     _dio.interceptors.add(JwtInterceptor(_dio));
   }
 
