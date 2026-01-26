fc41919741278a992001845d096ebfeb928955e1
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2026-01-26 10:09:05 +0700

    fix: debug mode build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,18 +1,9 @@
-import java.util.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
-}
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -40,30 +31,11 @@ android {
         versionName = "1.0.3"
     }
 
-    signingConfigs {
-        create("release") {
-            if (keystorePropertiesFile.exists()) {
-                storeFile = file(keystoreProperties["storeFile"] as String)
-                storePassword = keystoreProperties["storePassword"] as String
-                keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
-            }
-        }
-    }
-
     buildTypes {
         release {
-            signingConfig =
-                if (keystorePropertiesFile.exists())
-                    signingConfigs.getByName("release")
-                else
-                    signingConfigs.getByName("debug")
-
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
-        debug {
-            signingConfig = signingConfigs.getByName("debug")
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
