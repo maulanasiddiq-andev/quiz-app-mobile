3f4f57d5b0a40207622a5ae52eaf95c15e0a92e4
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2026-03-31 11:12:59 +0700

    fix: update package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.quiz_app"
+    namespace = "com.maulanasidiqdev.everyday_quiz"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.quiz_app"
+        applicationId = "com.maulanasidiqdev.everyday_quiz"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
