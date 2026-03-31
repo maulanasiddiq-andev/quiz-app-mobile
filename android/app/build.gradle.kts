163293dbdb6fa4418510bdbdf260ccc3383c253d
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2026-03-31 21:19:46 +0700

    fix: change versionCode

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 2
         versionName = "2.0.0"
     }
 
